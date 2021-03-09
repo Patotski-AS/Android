c73d2bef1cc2fe34452b013d3bc4285491d0914f
Andrey Patotski <7906241@gmail.com>
2021-03-09 20:20:04 +0300

    create LoginActivity

--- Kotlin/CafeOrder/app/src/main/java/com/example/android/cafeorder/LoginActivity.kt
+++ Kotlin/CafeOrder/app/src/main/java/com/example/android/cafeorder/LoginActivity.kt
@@ -1,11 +1,36 @@
 package com.example.android.cafeorder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.android.cafeorder.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun onClickCreateOrder(view: View) {
+        /**
+         * if - если данные введены:
+         * intent - создаем намерение, для вызова новой активности
+         * intent.putExtra - сохраняем в intent данные, для передачи в новую активность
+         * startActivity(intent) - вызываем новую активность
+         * else - если не хватает данных выводим уведомление
+         */
+        if (binding.editTextName.text.isNotEmpty() && binding.editTextPassword.text.isNotEmpty()) {
+            intent = Intent(this, CreateOrderActivity::class.java)
+            intent.putExtra("editTextName", binding.editTextName.text.toString().trim())
+            intent.putExtra("editTextPassword", binding.editTextPassword.text.toString().trim())
+            startActivity(intent)
+        }else
+            Toast.makeText(this,R.string.warning_fill_fiels,Toast.LENGTH_SHORT).show()
     }
 }
