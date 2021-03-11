e0e427cda5a7a91fb44b8f3e31031a12be8c5cc4
Andrey Patotski <7906241@gmail.com>
2021-03-11 18:48:14 +0300

    Create DrillDetailActivity

--- Kotlin/ToolsShop/app/src/main/java/com/example/android/toolsshop/DrillDetailActivity.kt
+++ Kotlin/ToolsShop/app/src/main/java/com/example/android/toolsshop/DrillDetailActivity.kt
@@ -2,10 +2,21 @@ package com.example.android.toolsshop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android.toolsshop.databinding.ActivityDrillDetailBinding
 
 class DrillDetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDrillDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_drill_detail)
+
+        binding = ActivityDrillDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.textViewInfo.text = intent.getStringExtra("info")
+        binding.textViewTitle.text = intent.getStringExtra("title")
+        binding.imageViewConcreteDrill.setImageResource(intent.getIntExtra("imageResourceId",-1))
+
     }
 }
