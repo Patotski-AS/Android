d780e526dd040bfd0725070914c0e855f2a8ab8d
Andrey Patotski <7906241@gmail.com>
2021-03-12 14:25:12 +0300

    create  class DownloadTask : AsyncTask<String, Unit, String>()

--- Kotlin/DownloadWebKontent/app/src/main/java/com/example/android/downloadwebkontent/MainActivity.kt
+++ Kotlin/DownloadWebKontent/app/src/main/java/com/example/android/downloadwebkontent/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.android.downloadwebkontent
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.net.HttpURLConnection
+import java.net.URL
+import kotlin.text.StringBuilder as StringBuilder
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val mailRu = "https://mail.ru/"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val task = DownloadTask()
+
+        val result = task.execute(mailRu).get()
+        Log.i("URL", result)
+
+    }
+
+    class DownloadTask : AsyncTask<String, Unit, String>() {
+
+        override fun doInBackground(vararg params: String?): String {
+            val result = StringBuilder()
+            var url: URL? = null
+            var urlConnection: HttpURLConnection? = null
+            try {
+                url = URL(params[0])
+                urlConnection = url.openConnection() as HttpURLConnection
+                val reader = InputStreamReader(urlConnection.inputStream)
+                val bufferReader = BufferedReader(reader)
+                var line = bufferReader.readLine()
+                while (line != null) {
+                    result.append(line)
+                    line = bufferReader.readLine()
+                }
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                urlConnection?.disconnect()
+
+                return result.toString()
+            }
+        }
     }
 }
