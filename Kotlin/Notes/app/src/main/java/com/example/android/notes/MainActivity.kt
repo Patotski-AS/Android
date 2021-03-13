001d8f746eda3792610934274f7b1eb8907e067a
Andrey Patotski <7906241@gmail.com>
2021-03-13 19:04:05 +0300

    create class Note and note_item.xml(макет)

--- Kotlin/Notes/app/src/main/java/com/example/android/notes/MainActivity.kt
+++ Kotlin/Notes/app/src/main/java/com/example/android/notes/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.android.notes
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.android.notes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private  var notes = arrayListOf<Note>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(notes) {
+           add(Note("Парикмахер", "Сделать прическу", "Понедельник", 2))
+           add(Note("Баскетбол", "Игра со школьной командой", "Вторник", 3))
+           add(Note("Магазин", "Купить новые джинсы", "Понедельник", 3))
+           add(Note("Стоматолог", "Вылечить зубы", "Понедельник", 2))
+           add(Note("Парикмахер", "Сделать прическу к выпускному", "Среда", 1))
+           add(Note("Баскетбол", "Игра со школьной командой", "Вторник", 3))
+           add(Note("Магазин", "Купить новые джинсы", "Понедельник", 3))
+        }
+
     }
 }
---
+++ Kotlin/Notes/app/src/main/java/com/example/android/notes/Note.kt
@@ -0,0 +1,9 @@
+package com.example.android.notes
+
+class Note(
+    val title: String,
+    val description: String,
+    val dayOfWeek: String,
+    val priority: Int
+) {
+}
