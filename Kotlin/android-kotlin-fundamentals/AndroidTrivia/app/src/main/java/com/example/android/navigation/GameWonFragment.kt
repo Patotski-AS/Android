f69575f63ee6748557313a3a29f53e0636ff5129
Andrey Patotski <7906241@gmail.com>
2021-04-11 13:40:24 +0300

    делимся результатами игры

--- Kotlin/android-kotlin-fundamentals/AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ Kotlin/android-kotlin-fundamentals/AndroidTrivia/app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,11 +16,10 @@
 
 package com.example.android.navigation
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -38,7 +37,7 @@ class GameWonFragment : Fragment() {
         //извлекаем аргументы из пакета, затем используйте a Toast для отображения аргументов
         val args = GameWonFragmentArgs.fromBundle(requireArguments())
 
-        Log.i("qwerty","NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}")
+        Log.i("qwerty", "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}")
 
         Toast.makeText(this.context,
                 "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}",
@@ -50,9 +49,58 @@ class GameWonFragment : Fragment() {
             view.findNavController()
                     .navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-
-
+        //МЕНЮ ПАРАМЕТРОВ
+        setHasOptionsMenu(true)
 
         return binding.root
     }
+
+    // Creating our Share Intent
+    private fun getShareIntent() : Intent {
+        val args = GameWonFragmentArgs.fromBundle(requireArguments())
+        val shareIntent = Intent(Intent.ACTION_SEND)
+        shareIntent.setType("text/plain")
+                .putExtra(Intent.EXTRA_TEXT, getString(R.string.share_success_text, args.numCorrect, args.numQuestions))
+        return shareIntent
+    }
+
+    /**
+     *Starting an Activity with our new Intent
+     * Этот метод получает Intentот getShareIntent()и вызывает,
+     * startActivity()чтобы начать совместное использование.
+      */
+    private fun shareSuccess() {
+        startActivity(getShareIntent())
+    }
+
+    /** Showing the Share Menu Item Dynamically
+     * *
+     * Используйте, getShareIntent()чтобы получить shareIntent.
+     * Чтобы убедиться, что shareIntent проблема разрешена Activity,
+     * обратитесь к диспетчеру пакетов Android ( PackageManager),
+     * который отслеживает приложения и действия, установленные на устройстве.
+     * Используйте package Manager свойство Activity,
+     * чтобы получить доступ к диспетчеру пакетов, и позвоните resolveActivity().
+     * Если результат равен null, что означает, что shareIntent проблема не разрешается,
+     * найдите пункт меню общего доступа в расширенном меню и сделайте этот пункт меню невидимым.
+     */
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.winner_menu, menu)
+        if(getShareIntent().resolveActivity(requireActivity().packageManager)==null){
+            menu.findItem(R.id.share).isVisible = false
+        }
+    }
+
+    /** Sharing from the Menu
+     * Чтобы обработать пункт меню, переопределите onOptionsItemSelected()в GameWonFragment классе
+     * Вызов shareSuccess()метода при щелчке по пункту меню:
+     */
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.share -> shareSuccess()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
