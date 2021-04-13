9908fc74e243bf11bf805e3b0e5bf9e4c2d374a9
Andrey Patotski <7906241@gmail.com>
2021-04-13 22:03:23 +0300

    add LiveData

--- Kotlin/android-kotlin-fundamentals/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ Kotlin/android-kotlin-fundamentals/GuessTheWord/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,15 +1,16 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
 
     // The current word
-     var word = ""
+    val word = MutableLiveData<String>()
 
     // The current score
-     var score = 0
+    val score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
@@ -18,6 +19,8 @@ class GameViewModel : ViewModel() {
      * Вызывается при создании класса
      */
     init {
+        word.value = ""
+        score.value = 0
         resetList()
         nextWord()
     }
@@ -67,21 +70,21 @@ class GameViewModel : ViewModel() {
      * Moves to the next word in the list
      */
     private fun nextWord() {
-        if (!wordList.isEmpty()) {
+        if (wordList.isNotEmpty()) {
             //Select and remove a word from the list
-            word = wordList.removeAt(0)
+            word.value = wordList.removeAt(0)
         }
     }
 
     /** Methods for buttons presses **/
 
      fun onSkip() {
-        score--
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
      fun onCorrect() {
-        score++
+        score.value = (score.value)?.minus(1)
         nextWord()
     }
 
