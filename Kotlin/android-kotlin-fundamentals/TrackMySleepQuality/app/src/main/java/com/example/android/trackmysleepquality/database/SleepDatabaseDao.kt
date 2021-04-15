b14166aaa73bc7df6dd9977ce8c23bf21b8bc6b8
Andrey Patotski <7906241@gmail.com>
2021-04-15 17:39:26 +0300

    Create the DAO

--- Kotlin/android-kotlin-fundamentals/TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ Kotlin/android-kotlin-fundamentals/TrackMySleepQuality/app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -16,7 +16,51 @@
 
 package com.example.android.trackmysleepquality.database
 
+import androidx.lifecycle.LiveData
 import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 
+/**
+ * объект доступа к данным (DAO)
+ */
 @Dao
-interface SleepDatabaseDao
+interface SleepDatabaseDao{
+
+    /**
+     * Room выполняет SQL-запрос для вставки объекта в базу данных
+     */
+    @Insert
+    fun insert(night: SleepNight)
+
+    /**
+     * Выберите все столбцы из daily_sleep_quality_table, где EnightId соответствует: key аргумент.
+     * Для ссылки на аргументы функции в запросе используется двоеточие
+     */
+    @Query("SELECT * from daily_sleep_quality_table WHERE nightId = :key")
+    fun get(key: Long): SleepNight?
+
+    /**
+     * Аннотация удаляет один элемент
+     * Недостатком является то, что вам нужно получить или узнать, что находится в таблице
+     */
+    @Query("DELETE FROM daily_sleep_quality_table")
+    fun clear()
+
+    /**
+     * SleepNight возвращаемое getTonight()функцией nullable,
+     * чтобы функция могла обрабатывать случай, когда таблица пуста.
+     * (Таблица пуста в начале и после очистки данных.)
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
+    fun getTonight(): SleepNight?
+
+    /**
+     * возвращает все столбцы из daily_sleep_quality_table, упорядоченные в порядке убывания.
+     *
+     */
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
+    fun getAllNights(): LiveData<List<SleepNight>>
+
+
+}
