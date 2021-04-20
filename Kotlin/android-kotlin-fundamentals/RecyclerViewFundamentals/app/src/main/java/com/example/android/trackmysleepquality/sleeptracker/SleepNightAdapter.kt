f5bfa52123f6eb9224da4a204011d85e9a8a71c8
Andrey Patotski <7906241@gmail.com>
2021-04-20 18:02:07 +0300

    Create binding adapters

--- Kotlin/android-kotlin-fundamentals/RecyclerViewFundamentals/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ Kotlin/android-kotlin-fundamentals/RecyclerViewFundamentals/app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -5,9 +5,6 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.trackmysleepquality.R
-import com.example.android.trackmysleepquality.convertDurationToFormatted
-import com.example.android.trackmysleepquality.convertNumericQualityToString
 import com.example.android.trackmysleepquality.database.SleepNight
 import com.example.android.trackmysleepquality.databinding.ListItemSleepNightBinding
 
@@ -39,18 +36,18 @@ class SleepNightAdapter : ListAdapter<SleepNight, SleepNightAdapter.ViewHolder>(
     class ViewHolder private constructor(val binding: ListItemSleepNightBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(item: SleepNight) {
-            val res = itemView.context.resources
-            binding.sleepLength.text = convertDurationToFormatted(item.startTimeMilli, item.endTimeMilli, res)
-            binding.qualityString.text = convertNumericQualityToString(item.sleepQuality, res)
-            binding.qualityImage.setImageResource(when (item.sleepQuality) {
-                0 -> R.drawable.ic_sleep_0
-                1 -> R.drawable.ic_sleep_1
-                2 -> R.drawable.ic_sleep_2
-                3 -> R.drawable.ic_sleep_3
-                4 -> R.drawable.ic_sleep_4
-                5 -> R.drawable.ic_sleep_5
-                else -> R.drawable.ic_sleep_active
-            })
+            /**
+             * назначьте sleep для item, потому что вам нужно сообщить объекту привязки о вашем новом SleepNight.
+             */
+            binding.sleep = item
+
+            /**
+             * Этот вызов является оптимизацией, которая требует от привязки данных
+             * немедленно выполнить любые ожидающие привязки. executePendingBindings()
+             * При использовании адаптеров привязки  всегда рекомендуется вызывать RecyclerView,
+             * потому что это может немного ускорить изменение размеров представлений.
+             */
+            binding.executePendingBindings()
         }
 
         companion object {
