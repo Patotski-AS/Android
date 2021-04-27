6066e2a46b65001e9d28849f481c812df2de6de1
Andrey Patotski <7906241@gmail.com>
2021-04-27 20:16:13 +0300

    fix

--- Kotlin/MyMovies/app/src/main/java/com/example/android/mymovies/mvvm/MovieRepository.kt
+++ Kotlin/MyMovies/app/src/main/java/com/example/android/mymovies/mvvm/MovieRepository.kt
@@ -3,7 +3,6 @@ package com.example.android.mymovies.mvvm
 import com.example.android.mymovies.AppConstants
 import com.example.android.mymovies.api.ApiService
 import com.example.android.mymovies.pogo.Movie
-import retrofit2.http.Query
 
 class MovieRepository(private val api: ApiService) : BaseRepository() {
 
@@ -14,7 +13,7 @@ class MovieRepository(private val api: ApiService) : BaseRepository() {
     ): MutableList<Movie>? {
         //safeApiCall is defined in BaseRepository.kt (https://gist.github.com/navi25/67176730f5595b3f1fb5095062a92f15)
         val movieResponse = safeApiCall(
-            call = { api.getMovies(language,sorted,page).await() },
+            call = { api.getMoviesAsync(language,sorted,page).await() },
             errorMessage = "Error Fetching Popular Movies"
         )
         return movieResponse?.results?.toMutableList();
